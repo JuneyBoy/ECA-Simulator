1d72ab506429ef85f9607bd5bb15c0ee396f66d2
JuneyBoy <arjun.ganesan@ou.edu>
2020-02-13 09:59:52 -0600

    More Testing
    Made some changes to the Driver Class and text documents to do more testing

--- Project 1/src/Driver.java
+++ Project 1/src/Driver.java
@@ -5,12 +5,9 @@ public class Driver {
 		boolean[] initState = new boolean[] {false, false, false, true, false, false, false};
 		boolean[] finalState = new boolean[] {true, false, true, true, false, false};
 		
-		Automaton test1 = new Automaton(222, initState);
-		test1.setFalseSymbol('t');
-		test1.setTrueSymbol('f');
-		test1.evolve(6);
-		String output = test1.toString();
-		System.out.println(output);
+		Automaton test1 = new Automaton("rule54-29cells-input.txt");
+		test1.evolve(30);
+		test1.save("rule54-29cells-output.txt");
 		//test1.evolve(7);
 		//test1.save("rule22-15cells-output");
 		
